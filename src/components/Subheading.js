dd8b2a8c0f1f1e15c549ea050074f7d7ecc6cd17
Gustavoadolfogjvv <Gustavoavilloldo@gmail.com>
2023-10-24 07:41:39 -0300

    new folder src

---
+++ src/components/Subheading.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Subheading extends Component {
+    render() {
+        return (
+            <P className='text-center lead'>Lista de tareas usando React</P>
+        );
+    }
+}
+
+export default Subheading;
